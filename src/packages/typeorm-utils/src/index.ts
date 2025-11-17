2a323594c187e85e5ebfadc3f14e05d1a0962e9c
homoakin619 <homohaykins619@gmail.com>
2025-11-17 04:06:08 +0100

    feat: added prisma orm support

--- src/packages/typeorm-utils/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './helpers/pagination.helper';
-export * from './abstract-model-action';
-export * from './options/create-record-generic';
-export * from './options/delete-record-generic';
-export * from './options/update-record-generic';
-export * from './options/save-record-generic';
